331fe619466f5c4705c16ab9aed13b544d597c95
munhyunsu <munhyunsu@gmail.com>
2019-04-14 09:41:25 +0900

    nav fix

--- mysite/xray/views.py
+++ mysite/xray/views.py
@@ -6,3 +6,8 @@ from django.shortcuts import render
 def index(request):
     if request.method == 'GET':
         return render(request, 'index.html')
+
+
+def archive(request):
+    if request.method == 'GET':
+        return render(request, 'archive.html')

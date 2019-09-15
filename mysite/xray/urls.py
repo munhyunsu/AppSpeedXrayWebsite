b33e763a5ae98cee452f5974cdef43a1b710591b
munhyunsu <munhyunsu@gmail.com>
2019-09-15 21:52:46 +0900

    serve media file

--- mysite/xray/urls.py
+++ mysite/xray/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
+from django.conf import settings
+from django.conf.urls.static import static
+
 from . import views
+
 
 router = routers.DefaultRouter()
 router.register(r'xray', views.XrayView)
@@ -13,4 +17,4 @@ urlpatterns = [
     path('submitreq', views.submitreq, name='submitreq'),
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
